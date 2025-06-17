2f18c0f6ba96c9c249007ffc60e98119a03a8c00
romnosk <romnosk@gmail.com>
2025-06-17 18:26:59 +0300

    ЗАдание 4.9. Создание схемы БД личного проекта

---
+++ schema.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE yeticave
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+
+USE yeticave;
+
+CREATE TABLE categories (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name CHAR(128) NOT NULL UNIQUE,
+  code CHAR(32) NOT NULL UNIQUE
+);
+
+CREATE TABLE lots (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name CHAR(128) NOT NULL UNIQUE,
+  description CHAR(255) NOT NULL UNIQUE,
+  image TEXT(512) NOT NULL,
+  start_price INT NOT NULL,
+  expire_date TIMESTAMP NOT NULL,
+  bet_step INT NOT NULL,
+  author_id INT NOT NULL,
+  winner_id INT,
+  category_id INT NOT NULL
+);
+
+CREATE TABLE bets (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  make_time TIMESTAMP NOT NULL,
+  start_price INT NOT NULL,
+  user_id INT NOT NULL,
+  lot_id INT NOT NULL
+);
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  reg_date TIMESTAMP NOT NULL,
+  email CHAR(128) NOT NULL UNIQUE,
+  name CHAR(255) NOT NULL,
+  password CHAR(128) NOT NULL,
+  contacts TEXT(1024)
+);
